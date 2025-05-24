019e9d4070eccb898aa5e1b71350ea02bff93f51
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-05-24 18:33:57 +0100

    correct

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,3 +3,15 @@ let toPrint =  function arguments(arg1){
 }
 
 toPrint("Arguments found")
+
+let print =  function arguments(arg2){
+    console.log(arg2);
+}
+
+print("Argument found")
+
+let allow =  function arguments(arg3){
+    console.log(arg3);
+}
+
+allow("No Argument")
