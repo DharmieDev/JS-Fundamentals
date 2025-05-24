ce52134bffa255ec75b008910656f61bdab17a33
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-05-24 18:28:49 +0100

    Updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,5 @@
-let toPrint =  function arguments(arg1, arg2, arg3){
+let toPrint =  function arguments(arg1){
     console.log(arg1);
-    console.log(arg2);
-    console.log(arg3);
 }
 
-toPrint("No argument", "Argument found","Arguments found")
+toPrint("Arguments found")
