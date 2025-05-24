faf8981bf2bf78457dc89a5682ba218550a09eed
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-05-24 18:22:40 +0100

    Arguments added

---
+++ 2-arguments.js
@@ -0,0 +1,7 @@
+let toPrint =  function arguments(arg1, arg2, arg3){
+    console.log(arg1);
+    console.log(arg2);
+    console.log(arg3);
+}
+
+toPrint("No argument", "Argument found","Arguments found")
