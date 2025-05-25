d464703efcc694c9c1f4903749cb28e8b1cdaae8
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-05-25 09:22:32 +0100

    Updated code

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,10 @@
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
+const argument = process.argv;
+if(argument.length === 4){
+  console.log("Arguments found")
+} else if(argument.length === 3){
+  console.log("Argument found")
+} else{
+  console.log("No arguments found")
 }
+
+
