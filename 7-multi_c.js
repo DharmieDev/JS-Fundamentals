6e6859d7c9b2b0bae51670dff71f897a46e0735a
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-05-25 21:47:45 +0100

    Updated

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,10 @@
 const arguments = process.argv[2];
 const x = parseInt(arguments)
-for(let i = 0; i < x; i++){
-        console.log("C is fun")
-    }
 if(isNaN(x)){
     console.log("Missing number of occurrences")
-} 
+} else{
+    for (let i = 0; i < x; i++){
+        console.log("C is fun")
+    }
+}
  
